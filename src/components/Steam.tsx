f1569444edf0dc16d8588a5812674c3cd42a74dd
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-04-12 14:34:05 +0700

    create steam

---
+++ src/components/Steam.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Chart, { Props } from "react-apexcharts";
+
+const getData = async () => {
+    const res = await fetch(`/api/dashboards/dailies`, {
+        cache: "no-store",
+    });
+    if (!res.ok) {
+        throw new Error("Failed!");
+    }
+
+    return res.json();
+};
+
+export const Steam = async () => {
+
+    const data = await getData()
+
+    const categories = data.map(item => item.date);
+
+    const state: Props["series"] = [
+        {
+            name: "Income",
+            data: [31, 40, 28, 51, 42, 109, 100],
+        },
+        {
+            name: "Expense",
+            data: [11, 32, 45, 32, 34, 52, 41],
+        },
+    ];
+
+    const options: Props["options"] = {
+        chart: {
+            type: "area",
+            animations: {
+                easing: "linear",
+                speed: 300,
+            },
+            sparkline: {
+                enabled: false,
+            },
+            brush: {
+                enabled: false,
+            },
+            id: "basic-bar",
+            foreColor: "hsl(var(--nextui-default-800))",
+            stacked: true,
+            toolbar: {
+                show: false,
+            },
+        },
+
+        xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+            labels: {
+                style: {
+                    colors: "hsl(var(--nextui-default-800))",
+                },
+            },
+            axisBorder: {
+                color: "hsl(var(--nextui-nextui-default-200))",
+            },
+            axisTicks: {
+                color: "hsl(var(--nextui-nextui-default-200))",
+            },
+        },
+        yaxis: {
+            labels: {
+                style: {
+                    colors: "hsl(var(--nextui-default-800))",
+                },
+            },
+        },
+        tooltip: {
+            enabled: false,
+        },
+        grid: {
+            show: true,
+            borderColor: "hsl(var(--nextui-default-200))",
+            strokeDashArray: 0,
+            position: "back",
+        },
+        stroke: {
+            curve: "smooth",
+            fill: {
+                colors: ["red"],
+            },
+        },
+        // @ts-ignore
+        markers: false,
+    };
+
+    return (
+        <>
+            <div className="w-full z-20">
+                <div id="chart">
+                    <Chart options={options} series={state} type="area" height={425} />
+                </div>
+            </div>
+        </>
+    );
+};
